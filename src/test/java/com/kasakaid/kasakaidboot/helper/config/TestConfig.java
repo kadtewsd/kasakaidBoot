aa520aaea35a3f3e2cc6c47cb70ad567291d553d
kasakaid <kasakaid@outlook.com>
2018-11-05 21:12:17 +0900

    単体テストで flyway に public スキーマが設定される問題を回避

--- src/test/java/com/kasakaid/kasakaidboot/helper/config/TestConfig.java
+++ src/test/java/com/kasakaid/kasakaidboot/helper/config/TestConfig.java
@@ -6,7 +6,6 @@ import com.kasakaid.kasakaidboot.helper.verify.ISimpleBean;
 import com.kasakaid.kasakaidboot.helper.verify.SimpleBean;
 import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +13,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 @Configuration
 @Profile("test")
-@TestConfiguration
 @ComponentScan(basePackageClasses = {ApplicationTests.class})
+//@TestPropertySource("classpath:application-test.properties")
 public class TestConfig {
 
     @Autowired
@@ -32,8 +34,14 @@ public class TestConfig {
 
     @Bean
     public TransactionAwareDataSourceProxy dataSource() {
+        net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy proxyDs = null;
+        if ("jdbc:log4jdbc:h2:mem:test;MODE=MySQL;DB_CLOSE_ON_EXIT=FALSE".equals(environment.getProperty("spring.datasource.url"))) {
+            EmbeddedDatabase ds = new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+            proxyDs = new net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy(ds);
+            return new TransactionAwareDataSourceProxy(proxyDs);
+        }
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        DataSourceSpy proxyDs = new net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy(dataSource);
+        proxyDs = new net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy(dataSource);
         // Autowired した environemnent からキーが発見できない。。
         // log4jdbc が何故かロードできない。
 //        dataSource.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
